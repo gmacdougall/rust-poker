7aad021784cfc29731b043aaca0c121f0ba55714
Gregor MacDougall <gmacdougall0+github@gmail.com>
2019-01-05 23:23:21 -0500

    Restructure file reading
    Using lines rather than a weird split makes more sense.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,39 @@
 mod poker;
 
-use std::io::{self, Read};
+use std::io;
+use std::io::prelude::*;
 
 use crate::poker::hand::Hand;
 
 fn main() -> io::Result<()> {
-    let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer)?;
-    for line in buffer.split("\n") {
-        if line.len() < 5 {
-            // FIXME: Last line of file
-            break;
-        }
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        match line {
+            Ok(line) => {
+                // FIXME: Unsafe unwrap
+                let hands: Vec<Hand> = line
+                    .split("|")
+                    .map(|h| Hand::parse(h).unwrap())
+                    .collect();
 
-        // FIXME: Unsafe unwrap
-        let hands: Vec<Hand> = line
-            .split("|")
-            .map(|h| Hand::parse(h).unwrap())
-            .collect();
-
-        match hands.iter().max() {
-            None => (),
-            Some(max) => {
-                println!(
-                    "{}, Winner: {}, Rank: {}",
-                    line,
-                    hands
-                        .iter()
-                        .filter(|h| h == &max)
-                        .map(|h| h.to_string())
-                        .collect::<Vec<String>>()
-                        .join(", "),
-                    max.rank(),
-                );
+                match hands.iter().max() {
+                    None => (),
+                    Some(max) => {
+                        println!(
+                            "{}, Winner: {}, Rank: {}",
+                            line,
+                            hands
+                                .iter()
+                                .filter(|h| h == &max)
+                                .map(|h| h.to_string())
+                                .collect::<Vec<String>>()
+                                .join(", "),
+                            max.rank(),
+                        );
+                    },
+                }
             },
+            Err(_) => panic!(),
         }
     }
     Ok(())
